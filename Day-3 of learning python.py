a4409d2a80a262df90b9698d1e3a89968653c4ac
Irfan5683 <145014229+Irfan5683@users.noreply.github.com>
2024-05-09 22:41:41 +0530

    Add files via upload

---
+++ Day-3 of learning python.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+f=("{first_place1}{second_place2}")
+print(f)
+
+
+# In[12]:
+
+
+first_name = "shaikh"
+last_name = "irfan"
+
+
+
+# In[16]:
+
+
+fullname=f"{first_name} {last_name}"
+print(fullname.title())
+
+
+# In[18]:
+
+
+message =f"keep up the good work {fullname.title()}"
+print(message)
+
+
+# In[19]:
+
+
+print("favorite_places:nandedmumbaidelhiagra")
+
+
+# In[20]:
+
+
+print("favorite_places:\nnanded\nmumbai\ndelhi\nagra")
+
+
+# In[22]:
+
+
+print("favorite_food:\n\tchicken\n\ttahari,briyani\n\ttalawa gosht")
+
+
+# In[26]:
+
+
+print("Life goals : I have very simple goles to achive\n\t1.To full fill them i move towards the programming\n\t2.learn to earn\n\t3.to make feel proud to parents\n\t4.prove myself that i can also do")
+
+
+# In[ ]:
+
+
+
+
